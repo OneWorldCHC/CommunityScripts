63332e51816b080a9f3ca22cd9d54f11e2153611
selgan <selgan@oneworldomaha.org>
2018-12-10 16:27:03 -0600

    Update Inject XS Tools Into WIM.ps1

--- Inject XS Tools Into WIM.ps1
+++ Inject XS Tools Into WIM.ps1
@@ -24,8 +24,73 @@
     https://stackoverflow.com/questions/20790798/start-process-wait-doesnt-work-when-script-is-launched-from-command-prompt-ope
     https://www.xenappblog.com/blog
     https://forums.veeam.com/veeam-agent-for-windows-f33/veeam-bare-metal-recovery-on-xenserver-with-pv-nic-drivers-t48304.html
+    http://blog.redit.name/posts/2015/powershell-loading-registry-hive-from-file.html
 #>
 
+Write-Verbose "Loading Functions" -Verbose
+
+Function Import-RegistryHive
+{
+    [CmdletBinding()]
+    Param(
+        [String][Parameter(Mandatory=$true)]$File,
+        # check the registry key name is not an invalid format
+        [String][Parameter(Mandatory=$true)][ValidatePattern('^(HKLM\\|HKCU\\)[a-zA-Z0-9- _\\]+$')]$Key,
+        # check the PSDrive name does not include invalid characters
+        [String][Parameter(Mandatory=$true)][ValidatePattern('^[^;~/\\\.\:]+$')]$Name
+    )
+
+    # check whether the drive name is available
+    $TestDrive = Get-PSDrive -Name $Name -EA SilentlyContinue
+    if ($TestDrive -ne $null)
+    {
+        throw [Management.Automation.SessionStateException] "A drive with the name '$Name' already exists."
+    }
+
+    $Process = Start-Process -FilePath "$env:WINDIR\system32\reg.exe" -ArgumentList "load $Key $File" -WindowStyle Hidden -PassThru -Wait
+
+    if ($Process.ExitCode)
+    {
+        throw [Management.Automation.PSInvalidOperationException] "The registry hive '$File' failed to load. Verify the source path or target registry key."
+    }
+
+    try
+    {
+        # validate patten on $Name in the Params and the drive name check at the start make it very unlikely New-PSDrive will fail
+        New-PSDrive -Name $Name -PSProvider Registry -Root $Key -Scope Global -EA Stop | Out-Null
+    }
+    catch
+    {
+        throw [Management.Automation.PSInvalidOperationException] "A critical error creating drive '$Name' has caused the registy key '$Key' to be left loaded, this must be unloaded manually."
+    }
+}
+
+Function Remove-RegistryHive
+{
+    [CmdletBinding()]
+    Param(
+        [String][Parameter(Mandatory=$true)][ValidatePattern('^[^;~/\\\.\:]+$')]$Name
+    )
+
+    # set -ErrorAction Stop as we never want to proceed if the drive doesnt exist
+    $Drive = Get-PSDrive -Name $Name -EA Stop
+    # $Drive.Root is the path to the registry key, save this before the drive is removed
+    $Key = $Drive.Root
+
+    # remove the drive, the only reason this should fail is if the reasource is busy
+    [gc]::collect() #added line to ensure registry properly unloads.
+
+    Remove-PSDrive $Name -EA Stop -Force
+    
+    $Process = Start-Process -FilePath "$env:WINDIR\system32\reg.exe" -ArgumentList "unload $Key" -WindowStyle Hidden -PassThru -Wait
+  
+    if ($Process.ExitCode)
+    {
+        # if "reg unload" fails due to the resource being busy, the drive gets added back to keep the original state
+        New-PSDrive -Name $Name -PSProvider Registry -Root $Key -Scope Global -EA Stop | Out-Null
+        throw [Management.Automation.PSInvalidOperationException] "The registry key '$Key' could not be unloaded, the key may still be in use."
+    }
+}
 
 #Declare variables
 #Change these to your liking.
@@ -45,6 +110,9 @@ $env:OSCDImgRoot = $OSCDIMGDir
 $DriverPaths = "$MountDir\Windows\system32\DriverStore\FileRepository\netrtl64.inf_amd64_8e9c2368fe308df2","$MountDir\Windows\WinSxS\amd64_netrtl64.inf_31bf3856ad364e35_10.0.16299.15_none_cc07f879fba34860","$MountDir\Windows\WinSxS\amd64_netrtl64.inf.resources_31bf3856ad364e35_10.0.16299.15_en-us_45ec42bc03abec68"
 $User = $env:USERDOMAIN +"\" + $env:USERNAME
 $PEDir = "$TempDir\WinPE"
+$TempRegPath = "HKLM\WinPE"
+$TempRegName = "WinPe"
+
 
 $StartDTM = (Get-Date)
 
@@ -121,6 +189,26 @@ ForEach ($Share in $MDTShares) {
     Write-Verbose "Injecting XenServer Tools into Mounted WIM" -Verbose
 
     Start-Process dism.exe -ArgumentList "/Image:$MountDir /Add-Driver /Driver:$ExtractDir\Citrix\XenTools /Recurse" -WindowStyle Hidden -Wait -Verbose
+
+    #Mount the following hive and give it a name of WinPE
+    Write-Verbose "Mounting Registry Hive" -Verbose
+    Import-RegistryHive -File $RegHive -Key $TempRegPath -Name WinPE -Verbose
+
+    New-ItemProperty -Path "WinPE:\ControlSet001\Control\Class\{4d36e96a-e325-11ce-bfc1-08002be10318}" -Name "UpperFilters" -PropertyType "MultiString" -Value "XENFILT" -Verbose | Out-Null
+
+    New-ItemProperty -Path "WinPe:\ControlSet001\Control\Class\{4d36e97d-e325-11ce-bfc1-08002be10318}" -Name 'UpperFilters' -PropertyType "MultiString" -Value "XENFILT" -Verbose | Out-Null
+
+    New-Item -Path "WinPE:\ControlSet001\Services\XEN\Unplug" -Force -Verbose| Out-Null
+
+    New-ItemProperty -Path "WinPe:\ControlSet001\Services\XEN\Unplug" -Name "DISKS" -Value "1" -PropertyType "DWORD" -Verbose | Out-Null
+
+    New-ItemProperty -Path "WinPe:\ControlSet001\Services\XEN\Unplug" -Name "NICS" -Value "1" -PropertyType "DWORD" -Verbose | Out-Null
+
+    New-ItemProperty -Path "WinPe:\ControlSet001\Services\xenfilt" -Name "WindowsPEMode" -Value "1" -PropertyType "DWORD" -Verbose | Out-Null
+
+    #Unmount Registry Hive
+    Write-Verbose "Dismounting Registry Hive" -Verbose
+    Remove-RegistryHive -Name WinPE
 
     #Unmount the WIM
     Write-Verbose "Dismounting WIM File" -Verbose
